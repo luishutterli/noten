d6b536961bd5708eef40b58bade2ba73f61743c5
Luis Hutterli <lshutterli@gmail.com>
2024-07-05 20:02:27 +0200

    Added some icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebase';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     const [user] = useAuthState(auth);
@@ -39,8 +39,11 @@ const Header = () => {
                 )}
                 {dropdownOpen && (
                     <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
+                        <button type="button" className="w-full block px-4 py-2 text-sm text-start text-blue-500 hover:bg-gray-200">
+                            <FontAwesomeIcon icon={faCog} className="mr-2" /> Settings
+                        </button>
                         <button type="button" className="w-full block px-4 py-2 text-sm text-start text-red-500 hover:bg-gray-200" onClick={handleLogout}>
-                            Logout
+                            <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" /> Logout
                         </button>
                     </div>
                 )}
